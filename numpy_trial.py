6ac6e5a40442bd058e6c84a2f3e62d32acbeb992
sashofthedark <sasha.bakman@gmail.com>
2021-01-25 22:08:53 +0200

    Some more numpy trials

--- numpy_trial.py
+++ numpy_trial.py
@@ -14,13 +14,17 @@ from numpy.testing._private.utils import jiffies
 #print(a_i)
 #print(b_i)
 
-v1 = np.array([[1,2]])
-v2 = np.array([2,4])
-cross_product = np.cross(v1,v2)
-print(cross_product)
+# v1 = np.array([[1,2]])
+# v2 = np.array([2,4])
+# cross_product = np.cross(v1,v2)
+# print(cross_product)
 
-newrow = [1,2,3]
-A = np.array([[1,3,4]])
-A = np.vstack([A, newrow])
-for row in A:
-    print(row)
+# newrow = [1,2,3]
+# A = np.array([[1,3,4]])
+# A = np.vstack([A, newrow])
+# for row in A:
+#     print(row)
+
+Matrix1 = np.array([[1,2,3],[2,4,5]])
+print(len(Matrix1[0,:]))
+print(len(Matrix1[:,0]))
